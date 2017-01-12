aae69eb01fa5a7faa0c8c08eea1db80f458ff577
UrLastWurdz <kwhoelscher@gmail.com>
2017-01-11 23:53:47 -0500

    Update rollDiceList.py

--- rollDiceList.py
+++ rollDiceList.py
@@ -1,11 +1,5 @@
 import random 
 
-
 def rollDice(numDice, maxVal):
     for x in range(numDice):
         rollRes.append(random.randint(1,maxVal))
-        
-
-rollRes=[]
-rollDice(6,6)
-print(rollRes)
