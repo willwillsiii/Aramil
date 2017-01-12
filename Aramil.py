52f1af06d0c2501952ddb9efe0aa43850df83ffb
willwillsiii <whwthree@gmail.com>
2017-01-11 21:01:22 -0500

    Beginning of basic python pipeline for Aramil
    Updated to include main functions and loading process

--- Aramil.py
+++ Aramil.py
@@ -1,75 +1,42 @@
 import discord
+import asyncio
+import logging
 import random
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 client = discord.Client()
 
-def roll():
-    if message.content.startswith("!roll"):
-        if message.content.endswith("d4"):
-            d4
-        if message.content.endswith("d6"):
-            d6
-        if message.content.endswith("d8"):
-            d8
-        if message.content.endswith("d10"):
-            d10
-        if message.content.endswith("d100"):
-            d100
-        if message.content.endswith("d12"):
-            d12
-        if message.content.endswith("d20"):
-            d20
+@client.event
+async def on_ready():
+	print('Logged in as')
+	print(client.user.name)
+	print(client.user.id)
+	print('------')
 
+@client.event
+async def on_message(message):
+	if message.content.startswith('!test'):
+		counter = 0
+		tmp = await client.send_message(message.channel, 'Calculating messages...')
+		async for log in client.logs_from(message.channel, limit=100):
+			if log.author == message.author:
+				counter += 1
+		await client.edit_message(tmp, 'You have {} messages.'.format(counter))
 
-            
-def d4():
-    if message.content.endswith("d4"):
-        num1 = randint(1,5)
-        client.send_message(message.channel,"you rolled a "+ num1)
-        
+	elif message.content.startswith('!sleep'):
+		await asyncio.sleep(5)
+		await client.send_message(message.channel, 'Done sleeping')
+		
+	elif message.content.startswith('!roll d6'):
+		rollRes = str(random.randint(1,6))
+		await client.send_message(message.channel, 'You rolled a ' + rollRes + '.')
 
-def d6():
-    if message.content.endswith("d6"):
-        num1 = randint(1,7)
-        client.send_message(message.channel,"you rolled a "+ num1)
-def d8():
-    if message.content.endswith("d8"):
-        num1 = randint(1,9)
-        client.send_message(message.channel,"you rolled a "+ num1)
-
-def d10():
-    if message.content.endswith("d10"):
-        num1 = randint(1,11)
-        client.send_message(message.channel,"you rolled a "+ num1)
-
-def d12():
-    if message.content.endswith("d12"):
-        num1 = randint(1,13)
-        client.send_message(message.channel,"you rolled a "+ num1)
-
-def d100():
-    if message.content.endswith("d10"):
-        num1 = randint(1,101,10)
-        client.send_message(message.channel,"you rolled a "+ num1)
-
-def d20():
-    if message.content.endswith("d20"):
-        num1 = randint(1,21)
-        client.send_message(message.channel,"you rolled a "+ num1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-
-client.login('Billsbestestfriend@gmail.com')
-client.run
+# Read token from text file
+with open('token.txt', 'r') as tokenFile:
+	token = tokenFile.read().replace('\n', '') # remove new-line chars
+client.run(token)
