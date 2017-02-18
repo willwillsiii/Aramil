9b907df6b980e032ea7978a10902e0dee4681e5d
willwillsiii <whwthree@gmail.com>
2017-02-17 23:36:08 -0500

    Add files via upload

--- dbot/dice.py
+++ dbot/dice.py
@@ -4,6 +4,17 @@ import re
 
 @singledispatch
 def rollDice(maxVal, numDice = 1):
+    
+    if maxVal <= 0 or not isinstance(maxVal, int):
+        raise ValueError(str(maxVal) + '-sided die does not have a positive integer number of faces.')
+    if numDice <= 0 or not isinstance(numDice, int):
+        raise ValueError(str(numDice) + ' dice is not a positive integer number of dice.')
+    
+    if maxVal > 200:
+        raise ValueError(str(maxVal) + '-sided die is too large. Maximum sides is 200.')
+    if numDice > 100:
+        raise ValueError(str(numDice) + ' dice is too many. Only roll up to 100 dice at once.')
+        
     rollRes = []
     for x in range(numDice):
         rollRes.append(random.randint(1, maxVal))
@@ -19,8 +30,14 @@ def _(rollStr):
             maxVal = rollList[i].split('d')[1]
             if numDice == '': numDice = '1'
             if maxVal == '': maxVal = '6'
+            if '.' in numDice:
+                raise ValueError("invalid literal numDice=" + numDice +
+                                 " for int() with base 10: '" + numDice + "'")
+            if '.' in maxVal:
+                raise ValueError("invalid literal maxVal=" + maxVal +
+                                 " for int() with base 10: '" + maxVal + "'")
             rollList[i] = str(sum(rollDice(int(maxVal),int(numDice))))
-        return eval(''.join(rollList))
+    return eval(''.join(rollList))
 
 def chatRoll(msg):
     msg = msg.split(' ', 1)[1]
