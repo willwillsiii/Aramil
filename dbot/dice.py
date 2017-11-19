91465a4c9b1131c4971d8e3fb91e02d62a399712
Will Wills <whwthree@gmail.com>
2017-11-19 02:43:46 -0500

    mend

--- dbot/dice.py
+++ dbot/dice.py
@@ -114,9 +114,9 @@ def chat_roll(roll_str, verbose=False, formatted=False):
     for roll in chat_list:
         if '||' in roll:
             roll_str, indv_comment = tuple(roll.split('||', 1))
-            rolls.append(''.join[
+            rolls.append(''.join([
                 chat_roll_single(roll_str, verbose, formatted),
-                ': ', indv_comment])
+                ': ', indv_comment]))
         else:
             rolls.append(chat_roll_single(roll, verbose, formatted))
     return (comment.strip() + "\n" +
