4c1e3a0f3dd7b95ac9285a738aeba038a2b820b0
Will Wills <whwthree@gmail.com>
2017-11-19 02:41:00 -0500

    mend

--- aramil.py
+++ aramil.py
@@ -86,10 +86,7 @@ async def on_message(message):
             raise
         finally:
             # prepend mention
-            if '\n' in roll_msg:
-                roll_msg = ''.join([message.author.mention, ": \n", roll_msg])
-            else:
-                roll_msg = ''.join([message.author.mention, ": ", roll_msg])
+            roll_msg = ''.join([message.author.mention, ":\n", roll_msg])
             await client.send_message(message.channel, roll_msg)
 
 # Begin Execution
